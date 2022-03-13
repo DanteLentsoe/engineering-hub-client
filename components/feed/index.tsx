423d9bcaa846d2aff9070a6983685e2018c624dd
dantelentsoe <dllentsoe@gmail.com>
2022-03-13 22:05:24 +0200

    refactor: scroll bar and dependencies

--- components/feed/index.tsx
+++ components/feed/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SharedFeed from '../sharedFeed'
 const Feed = () => {
   return (
-    <main className="md:grid-clos-2 max-auto grid grid-cols-2 md:max-w-3xl xl:max-w-6xl xl:grid-cols-3">
+    <main className="md:grid-clos-2 mx-auto grid grid-cols-2 md:max-w-3xl xl:max-w-6xl xl:grid-cols-3">
       <section className="col-span-2">
         <SharedFeed />
       </section>
--- components/header/styles.ts
+++ components/header/styles.ts
@@ -1,4 +1,3 @@
-import styled from 'styled-components'
 import tw from 'tailwind-styled-components'
 
 export const HeaderContainer = tw.div`
