ea87efa9c1f702decd0967c651e287c86872e030
dantelentsoe <dllentsoe@gmail.com>
2022-03-19 09:07:38 +0200

    feat: pull user image and user name

--- components/profileGlobal/index.tsx
+++ components/profileGlobal/index.tsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const ProfileGlobal = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-      <img
-        className="rounded-full border p-[2px] w-16 h-16"
-        src="https://avatars.githubusercontent.com/u/65385487?v=4"
-        alt="engineering hub loggedin user profile picture"
-      />
-      <div className="flex-1 mx-4">
-        <h2 className="font-bold">Dante Lentsoe</h2>
-        <h3 className="text-sm text-gray-500 ">Welcome to Engineering Hub</h3>
-      </div>
-      <button className="text-teal-800 text-sm font-bold">Sign out</button>
+      {session && (
+        <>
+          <img
+            className="rounded-full border p-[2px] w-16 h-16"
+            src={session?.user?.image}
+            alt="engineering hub loggedin user profile picture"
+          />
+          <div className="flex-1 mx-4">
+            <h2 className="font-bold">{session?.user?.name}</h2>
+            <h3 className="text-sm text-gray-500 ">
+              Welcome to Engineering Hub
+            </h3>
+          </div>
+
+          <button className="text-teal-800 text-sm font-bold" onClick={signOut}>
+            Sign out
+          </button>
+        </>
+      )}
     </div>
   );
 };
--- components/sharedFeed/index.tsx
+++ components/sharedFeed/index.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import EngineeringFeed from "../engineeringFeed";
 import { SharedFeedContainer } from "./styles";
-
+import { useSession } from "next-auth/react";
 const SharedFeed = () => {
   const [feedInfo, setFeedInfo] = useState<[] | Array<object>>([]);
+
+  const { data: session } = useSession();
+
   useEffect(() => {
     //load set data
-
     const feedInfoFaker = [...Array(16)].map((_, index) => ({
       ...faker.helpers.contextualCard(),
       id: index,
@@ -18,6 +20,12 @@ const SharedFeed = () => {
 
   return (
     <SharedFeedContainer>
+      {session && (
+        <EngineeringFeed
+          img={session?.user?.image}
+          username={session?.user?.name}
+        />
+      )}
       {feedInfo.map((userProfile: any) => (
         <EngineeringFeed
           key={userProfile.id}
