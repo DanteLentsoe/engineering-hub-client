b590917e9237a7cd20c0e51e4426041dde8303b5
dantelentsoe <dllentsoe@gmail.com>
2022-03-19 20:03:51 +0200

    refactor: reposition file

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -24,6 +24,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 
 const storage = getStorage();
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
-export { app, db, storage, analytics };
+export { app, db, storage };
